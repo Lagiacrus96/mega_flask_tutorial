d6000b0a5e737b7faeed17a8fd40dc33a9d16e1b
Matthew Meinema <mattmeinema@hotmail.com>
2023-01-27 15:39:13 +0100

    Chapter 3

---
+++ app/routes.py
@@ -0,0 +1,28 @@
+from app import app
+from app.forms import LoginForm
+from flask import render_template
+
+@app.route('/')             # These 2 lines are decorators
+@app.route('/index')
+def index():
+    user = {'username': 'Matthew'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Amsterdam!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Persona ports were so cool!'
+        },
+        {
+            'author': {'username': 'Jesus'},
+            'body': 'I have a tiny willy!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Sign In', form=form)
