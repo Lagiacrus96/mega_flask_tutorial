be16425bc16cb297e95e9b22d1163fef7e491864
Matthew Meinema <mattmeinema@hotmail.com>
2023-01-27 17:46:06 +0100

    Chapter 3 Complete

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 from app import app
 from app.forms import LoginForm
 
@@ -22,7 +22,15 @@ def index():
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST']) # GET requests return information to browser. POST requests give information to server
 def login():
     form = LoginForm()
+    if form.validate_on_submit(): # Will run when browser sends POST request. After all validators work, will return True
+        flash('Login request for user {}, remember_me{}'.format( # flash is a Flask function that shows user a message
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('/index'))
+
     return render_template('login.html', title='Sign In', form=form)
+
+if __name__ == "__main__":
+    app.run(debug=True)
