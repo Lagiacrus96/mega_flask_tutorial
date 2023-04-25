fbbd436799d7f5c9a0b5d88b525d568bf7437ca0
Matthew Meinema <mattmeinema@hotmail.com>
2023-04-25 15:03:56 +0200

    Chapter 6

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,16 @@
 from flask import render_template, flash, redirect, url_for, request
 from app import app, db
-from app.forms import LoginForm, RegistrationForm
+from app.forms import LoginForm, RegistrationForm, EditProfileForm
 from flask_login import current_user, login_user, logout_user, login_required
 from app.models import User
 from werkzeug.urls import url_parse
+from datetime import datetime
+
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()
 
 @app.route('/')             # These 2 lines are decorators
 @app.route('/index')
@@ -62,6 +69,32 @@ def register():
         flash('Congratulations! You are now a registered user!')
         return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
+
+@app.route('/user/<username>') # < > denotes dynamic component
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404() # Sends a 404 if username is not found in DB
+    posts = [
+        {'author': user, 'body': 'Test post #1'},
+        {'author': user, 'body': 'Test post #2'}
+    ]
+    return render_template('user.html', user=user, posts=posts)
+
+@app.route('/edit_profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.about_me = form.about_me.data
+        db.session.commit()
+        flash(' Your changes have been saved! ')
+        return redirect(url_for('edit_profile'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.about_me.data = current_user.about_me
+    return render_template('edit_profile.html', title='Edit Profile',
+                           form = form)
 
 if __name__ == "__main__":
     app.run(debug=True)
